817296a2b3e3d7f3ad4529adf2b8431f3f914684
baaami <lgh121546@naver.com>
2024-04-06 11:18:35 +0900

    http server project setting

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Self {
+            addr
+        }
+    }
+
+    fn run(self) {
+        println!("Listening on {}", self.addr);
+    }
+}
+
+struct Request {
+    path: String,
+    query_string: String,
+    method: String,
+}
+
+/*
+GET /user?id=10 HTTP/1.1\r\n
+HEADERS \r\n
+BODY
+*/
