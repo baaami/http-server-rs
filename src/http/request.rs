41b6cd6581347f5e96d178f8d36b75529ce3e66a
baaami <lgh121546@naver.com>
2024-04-10 10:51:36 +0900

    1. 유입 요청 수신 및 출력 2. 수신한 버퍼를 특정 타입으로 변환 중

--- src/http/request.rs
+++ src/http/request.rs
@@ -6,3 +6,11 @@ pub struct Request {
     // super는 Parent Module를 참조할 수 있음
     method: Method,
 }
+
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!()
+    }
+}
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,6 @@
-use std::net::TcpListener;
+use crate::http::request::Request;
+use std::convert::TryFrom;
+use std::{io::Read, net::TcpListener};
 
 pub struct Server {
     addr: String,
@@ -15,5 +17,31 @@ impl Server {
         println!("Listening on {}", self.addr);
 
         let listener = TcpListener::bind(&self.addr).unwrap();
+        
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer: [u8; 1024] = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                            // = Request::try_from(&buffer[..]);
+                            match Request::try_from(&buffer as &[u8]) {
+                                Ok(request) => {
+
+                                },
+                                Err(e) => println!("Failed to parse a request: {}", e),
+                            }
+                        },
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                }
+                Err(e) => println!("Failed to establish a connection: {}", e),
+                
+                // default와 같은 역할, match에 enum과 같은 type이 할당되었을 경우 default 역할을함
+                // _ => println!("Unkown"), 
+            }
+        }
     }
 }
